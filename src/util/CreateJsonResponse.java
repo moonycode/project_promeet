3351e3206542ee1aad9ae5ce1bae6364704906cf
shpark091 <shpark091@gmail.com>
2025-10-15 17:46:39 +0900

    feat(util) : add CreateJsonResponse

---
+++ src/util/CreateJsonResponse.java
@@ -0,0 +1,11 @@
+package util;
+
+import com.google.gson.Gson;
+
+public class CreateJsonResponse {
+    
+    public static String toJson(Object data) {
+        Gson gson = new Gson();
+        return gson.toJson(data);
+    }
+}
